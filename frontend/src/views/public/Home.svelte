b3a328da68a1743e2b71159d24614dbfcdf3103f
Eva Maciejko <emaciejko@gmail.com>
2022-08-13 21:56:27 -0400

    Early version of the landing page

--- frontend/src/views/public/Home.svelte
+++ frontend/src/views/public/Home.svelte
@@ -1,3 +1,51 @@
-<div>
-  <h1 class="title is-4">Home</h1>
+<div class="notification">
+  <div class="columns">
+    <div class="column">
+      <div class="content">
+        <h1>Publiez facilement vos menus scolaires</h1>
+        <p>
+          Nous facilitons la publication de vos menus scolaires hebdomadaires sur le web et les fournissons aux élèves et aux parents dans un format adapté au mobile.
+        </p>
+        <p>
+          Entrez seulement les détails du menu et pressez publiez ! Enregistrez-vous et débutez dès aujourd'hui.
+        </p>
+      </div>
+    </div>
+    <div class="column">
+      <div class="content">
+        <h3>Enregistrez-vous maintenant</h3>
+        <div class="field">
+          <label class="label">Adresse courriel</label>
+          <div class="control">
+            <input
+              type="text"
+              placeholder="Entrez votre adresse courriel"
+              class="input" />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">Nom de l'école</label>
+          <div class="control">
+            <input
+              type="text"
+              placeholder="Entrez le nom de l'école"
+              class="input" />
+          </div>
+        </div>
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-link is-outlined">Go</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="content" style="line-height: 30px;">
+    <center>
+      <span>Déjà enregistré ?</span>
+      <button class="button is-small is-text">Connectez-vous</button>
+    </center>
+  </div>
 </div>
