123f940ca278f751cce96c5d8e790e731f8cce55
Eva Maciejko <emaciejko@gmail.com>
2022-08-13 21:49:37 -0400

    Added Bulma and some styling

--- frontend/src/views/admin/AdminLayout.svelte
+++ frontend/src/views/admin/AdminLayout.svelte
@@ -1,9 +1,25 @@
 <script>
   import { Route } from 'svelte-router-spa'
+  import { onMount } from 'svelte'
+  import { user } from '../../stores.js'
+
   export let currentRoute
+
+  let initialized = false
+
+   // The onMount lifecycle function
+   onMount(() => {
+    // Svelte stores are written to by calling set
+    user.set({
+      name: 'Usager Test',
+      schoolName: 'École Test',
+    })
+    initialized = true
+  })
 </script>
 
-<div>
-  Admin Layout
+<div>  
+  {#if initialized}
   <Route {currentRoute} />
+  {/if}
 </div>
--- frontend/src/views/admin/LunchMenuAdmin.svelte
+++ frontend/src/views/admin/LunchMenuAdmin.svelte
@@ -1,7 +1,24 @@
 <script>
-  let schoolName = 'Test School'
+  import { user } from '../../stores.js'
 </script>
 
 <div>
-  <h1 class="title is-4">Lunch Menu Administration / {schoolName}</h1>
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/admin/manage-menus">Administration des Menus</a>
+      </li>
+      <li class="is-active">
+        <a href="/#">{$user.schoolName}</a>
+      </li>
+    </ul>
+  </nav>
+</div>
+<div class='section'>
+  <div class='container'>
+  <h1 class="title is-4">Administration des Menus / {$user.schoolName}</h1>
+  </div>
 </div>
