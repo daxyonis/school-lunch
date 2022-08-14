9bbde3dd8683520b353cf803bcb75e35bed1aca6
Eva Maciejko <emaciejko@gmail.com>
2022-08-13 21:37:48 -0400

    Fix prettier setting

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,7 @@
-import Home from './views/public/Home.svelte'
-import LunchMenuView from './views/public/LunchMenuView.svelte'
-import LunchMenuAdmin from './views/admin/LunchMenuAdmin.svelte'
-import AdminLayout from './views/admin/AdminLayout.svelte'
+import Home from './views/public/Home.svelte';
+import LunchMenuView from './views/public/LunchMenuView.svelte';
+import LunchMenuAdmin from './views/admin/LunchMenuAdmin.svelte';
+import AdminLayout from './views/admin/AdminLayout.svelte';
 
 const routes = [
   { name: '/', component: Home },
@@ -11,6 +11,6 @@ const routes = [
     component: AdminLayout,
     nestedRoutes: [{ name: 'index', component: LunchMenuAdmin }],
   },
-]
+];
 
-export { routes }
+export { routes };
