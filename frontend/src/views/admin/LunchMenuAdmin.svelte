7f8aef4d3cb8bb201c630cad4e6b59118d40870f
Eva Maciejko <emaciejko@gmail.com>
2022-08-19 16:16:33 -0400

    Added delete lunch week in frontend

--- frontend/src/views/admin/LunchMenuAdmin.svelte
+++ frontend/src/views/admin/LunchMenuAdmin.svelte
@@ -4,12 +4,14 @@
   import { navigateTo } from 'svelte-router-spa' 
   import axios from 'axios'
   import Icon from 'svelte-awesome'
-  import { refresh } from 'svelte-awesome/icons'
+  import { refresh, times } from 'svelte-awesome/icons'
 
   export let currentRoute
 
   let lunchWeekList = []
   let loading = true
+  let showDeleteModal = false
+  let weekToDelete = {}
 
   onMount(async () => {
     try {      
@@ -25,13 +27,42 @@
     const route = `/admin/manage-menus/week-details/${lunchWeek.lunchWeekId}`
     navigateTo(route)
   }
+
+  const openDeleteModal = (lunchWeek) => {
+    weekToDelete = lunchWeek
+    showDeleteModal = true
+  }
+
+  const deleteLunchWeek = async lunchWeek => {
+    showDeleteModal = false
+    const lunchWeekId = lunchWeek.lunchWeekId
+    try {
+      // show the loading spinner and call the delete endpoint
+      loading = true
+      await axios.delete(`${process.env.API_ROOT}/api/lunch-week/${lunchWeekId}`)
+
+      // find the index of the passed in lunchWeek and use splice to remove it
+      const deletedIndex = lunchWeekList.findIndex(x => x.lunchWeekId === lunchWeekId)
+      lunchWeekList.splice(deletedIndex, 1)
+      loading = false
+    } catch (e) {
+      loading = false
+      console.error(e)
+    }
+}
 </script>
+
+<style>
+  .clickable {
+    cursor: pointer;
+  }
+</style>
 
 <div>
   <nav class="breadcrumb" aria-label="breadcrumbs">
     <ul>
       <li>
-        <a href="/">Home</a>
+        <a href="/">Accueil</a>
       </li>
       <li>
         <a href="/admin/manage-menus">Administration des Menus</a>
@@ -49,16 +80,35 @@
   <table class="table">
     <thead>
       <tr>
-        <th>Week Of</th>
-        <th>Published</th>
+        <th>Semaine du</th>
+        <th>Publié</th>
+        <th></th>
       </tr>
     </thead>
     {#each lunchWeekList as lunchWeek}
-    <tr on:click="{openLunchWeekDetails(lunchWeek)}" class="has-text-link" style="cursor: pointer" >
-      <td>{lunchWeek.weekOf}</td>
+    <tr>
+      <td on:click="{openLunchWeekDetails(lunchWeek)}" class="has-text-link clickable">{lunchWeek.weekOf}</td>
       <td>{lunchWeek.isPublished}</td>
+      <td on:click="{openDeleteModal(lunchWeek)}" class="has-text-grey-light clickable">
+        <Icon style="margin-top: 4px;" data="{times}" />
+      </td>
     </tr>
     {/each}
   </table>
   {/if}
+</div>
+
+<div class="{showDeleteModal ? 'modal is-active' : 'modal'}">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Attention</p>
+      <button class="delete" on:click="{() => (showDeleteModal = false)}" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">Effacer la Semaine du {weekToDelete.weekOf}?</section>
+    <footer class="modal-card-foot">
+      <button class="button is-success" on:click="{deleteLunchWeek(weekToDelete)}">Oui, effacer</button>
+      <button class="button" on:click="{() => (showDeleteModal = false)}">Annuler</button>
+    </footer>
+  </div>
 </div>
--- frontend/src/views/admin/LunchMenuAdminDetails.svelte
+++ frontend/src/views/admin/LunchMenuAdminDetails.svelte
@@ -6,10 +6,10 @@
   <nav class="breadcrumb" aria-label="breadcrumbs">
     <ul>
       <li>
-        <a href="/">Home</a>
+        <a href="/">Accueil</a>
       </li>
       <li>
-        <a href="/admin/manage-menus">Lunch Menu Administration</a>
+        <a href="/admin/manage-menus">Administration des menus</a>
       </li>
       <li class="is-active">
         <a href="/#">{$user.schoolName}</a>
