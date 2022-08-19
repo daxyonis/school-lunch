024ac46c9fa7bca6abc3d3e20cdfd04756b04cc8
Eva Maciejko <emaciejko@gmail.com>
2022-08-19 14:51:16 -0400

    Updated backend API for CRUD operations on lunch weeks, lunch days

--- backend/routes/lunch-week.js
+++ backend/routes/lunch-week.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const knex = require('../database/client');
 
-// Create a helper function to select all the rows from the
-// lunch_week table
+//************************
+// Helper functions
 const getLunchWeekList = () => {
   return knex.select().from('lunch_week').orderBy('week_of');
 };
@@ -12,6 +12,30 @@ const getLunchWeekById = (id) => {
   return knex.select().from('lunch_week').where('lunch_week_id', id).first();
 };
 
+const createLunchWeek = (lunchWeek) => {
+  // for Postgres, we need to specifiy what columns to return back
+  // from the insert statement with a call to `.returning()`
+  return knex('lunch_week').insert(lunchWeek).returning('lunch_week_id');
+};
+
+const updateLunchWeek = (id, lunchWeek) => {
+  return knex('lunch_week').where('lunch_week_id', id).update(lunchWeek);
+};
+
+const deleteLunchWeek = (lunchWeekId) => {
+  return knex('lunch_week').where('lunch_week_id', lunchWeekId).del();
+};
+
+const createLunchDay = (lunchDay) => {
+  return knex('lunch_day').insert(lunchDay).returning('lunch_day_id');
+};
+
+const updateLunchDay = (lunchDayId, lunchDay) => {
+  return knex('lunch_day').where('lunch_day_id', lunchDayId).update(lunchDay);
+};
+//*********************************
+
+// Get all lunch weeks
 router.get('/', async function (req, res) {
   try {
     const lunchWeekList = await getLunchWeekList();
@@ -24,6 +48,7 @@ router.get('/', async function (req, res) {
   }
 });
 
+// Get one lunch week
 router.get('/:lunchWeekId', async function (req, res) {
   try {
     const id = parseInt(req.params.lunchWeekId);
@@ -45,4 +70,92 @@ router.get('/:lunchWeekId', async function (req, res) {
   }
 });
 
+// create lunch week
+router.post('/', async function (req, res) {
+  const lunchWeek = req.body;
+  try {
+    const insertResponse = await createLunchWeek(lunchWeek);
+    // Since you can insert more than one row with `knex.insert`, the response is
+    // an array, so we need to return the 0 position
+    const insertedLunchWeekId = insertResponse[0];
+    const response = {
+      lunchWeekId: insertedLunchWeekId,
+    };
+    res.send(response);
+  } catch (e) {
+    const message = `Error creating Lunch Week`;
+    res.status(500).send({ message: message, error: e.toString() });
+  }
+});
+
+// Update lunch week
+router.put('/:lunchWeekId', async function (req, res) {
+  try {
+    const id = parseInt(req.params.lunchWeekId);
+    const lunchWeek = req.body;
+
+    if (id !== lunchWeek.lunchWeekId) {
+      const message = `Bad request, IDs do not match`;
+      res.status(400).send({ message: message });
+      // IMPORTANT, we need to explicitly return here, otherwise the rest
+      // of the endpoint code will continue to run.
+      // In other words, res.send does not return like you might think it would
+      return;
+    }
+
+    await updateLunchWeek(id, lunchWeek);
+    const updatedLunchWeek = await getLunchWeekById(id);
+    res.send(updatedLunchWeek);
+  } catch (e) {
+    const message = `Error updating Lunch Week`;
+    res.status(500).send({ message: message, error: e.toString() });
+  }
+});
+
+// Delete lunch week
+router.delete('/:lunchWeekId', async function (req, res) {
+  try {
+    const id = parseInt(req.params.lunchWeekId);
+    await deleteLunchWeek(id);
+    res.send();
+  } catch (e) {
+    const message = `Error deleting Lunch Week`;
+    res.status(500).send({ message: message, error: e.toString() });
+  }
+});
+
+// Create lunch day
+router.post('/:lunchWeekId/lunch-day', async function (req, res) {
+  const lunchDay = req.body;
+  try {
+    const insertResponse = await createLunchDay(lunchDay);
+    const insertedLunchDayId = insertResponse[0];
+    const response = {
+      lunchDayId: insertedLunchDayId,
+    };
+    res.send(response);
+  } catch (e) {
+    const message = `Error creating Lunch Day`;
+    res.status(500).send({ message: message, error: e.toString() });
+  }
+});
+
+// Update lunch day
+router.put('/:lunchWeekId/lunch-day/:lunchDayId', async function (req, res) {
+  try {
+    const lunchDayId = parseInt(req.params.lunchDayId);
+    const lunchDay = req.body;
+
+    if (lunchDayId !== lunchDay.lunchDayId) {
+      const message = `Bad request, IDs do not match`;
+      res.status(400).send({ message: message });
+      return;
+    }
+    await updateLunchDay(lunchDayId, lunchDay);
+    res.send();
+  } catch (e) {
+    const message = `Error updating Lunch Day`;
+    res.status(500).send({ message: message, error: e.toString() });
+  }
+});
 module.exports = router;
