2d8724941e90862872e2eead0c6eb9337d81d7b4
Eva Maciejko <emaciejko@gmail.com>
2022-08-19 12:17:26 -0400

    Added knex and pg for persistence

---
+++ backend/migrations/20220819161027_create_lunch_week_table.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  // creates the lunch_week table
+  return knex.schema.createTable('lunch_week', function (table) {
+    table.increments('lunch_week_id'); // auto-numbering primary key column
+    table.date('week_of').notNullable();
+    table.boolean('is_published').notNullable().defaultTo(false);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  // if we wanted to undo this migration, then we would drop the table
+  return knex.schema.dropTable('lunch_week');
+};
---
+++ backend/migrations/20220819161512_create_lunch_day_table.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('lunch_day', function (table) {
+    table.increments('lunch_day_id'); // primary key
+    table.integer('lunch_week_id').notNullable(); // create the foreign key (FK) column first
+    table.date('day').notNullable();
+    table.string('menu_details', 1000);
+    table
+      .foreign('lunch_week_id')
+      .references('lunch_week_id')
+      .inTable('lunch_week') // FK to the parent lunch_week table
+      .onDelete('CASCADE'); // If the parent lunch_week row is deleted, delete any child lunch_day rows
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('lunch_day');
+};
