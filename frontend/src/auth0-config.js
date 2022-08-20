effd5efbf7c1dfbeb451c0afc007865ab488f321
Eva Maciejko <emaciejko@gmail.com>
2022-08-20 07:32:28 -0400

    Add Auth0 for frontend login

---
+++ frontend/src/auth0-config.js
@@ -0,0 +1,8 @@
+// src/auth0-config.js
+const auth0Config = {
+  audience: 'https://evkosoft.auth0.com/api/v2/',
+  domain: 'evkosoft.auth0.com',
+  client_id: 'JomZY9Upy4YaLdycBsW1uOY69Mx7KMoE',
+  cacheLocation: 'localstorage',
+};
+export default auth0Config;
--- frontend/src/views/admin/AdminLayout.svelte
+++ frontend/src/views/admin/AdminLayout.svelte
@@ -2,24 +2,43 @@
   import { Route } from 'svelte-router-spa'
   import { onMount } from 'svelte'
   import { user } from '../../stores.js'
+  import createAuth0Client from '@auth0/auth0-spa-js'
+  import auth0Config from '../../auth0-config'
 
   export let currentRoute
 
   let initialized = false
+  let auth0 
 
    // The onMount lifecycle function
-   onMount(() => {
-    // Svelte stores are written to by calling set
-    user.set({
-      name: 'Usager Test',
-      schoolName: 'École Test',
-    })
-    initialized = true
+   onMount( async () => {
+    auth0 = await createAuth0Client(auth0Config)
+    const authenticated = await auth0.isAuthenticated()
+
+    if (!authenticated) {
+      await auth0.loginWithRedirect({
+        redirect_uri: `${window.location.origin}/callback`,
+        appState: window.location.pathname,
+      })
+    } else {
+      user.set({
+        name: 'Test User',
+        schoolName: 'Test School',
+      })
+      initialized = true
+    }   
   })
+
+  const logout = async () => {
+    auth0.logout({
+      returnTo: window.location.origin,
+    })
+  }
 </script>
 
 <div>  
   {#if initialized}
-  <Route {currentRoute} />
+    <button class="button is-light is-small" style="float: right;" on:click="{() => logout()}">Logout</button>
+    <Route {currentRoute} />
   {/if}
 </div>
