77a8df6bb2073758080dd52b55db806f1b659169
Eva Maciejko <emaciejko@gmail.com>
2022-08-27 16:46:02 -0400

    Better dockerfile and run script

--- backend/docker_run.bat
+++ backend/docker_run.bat
@@ -1,1 +1,1 @@
-docker run --name school-lunch -p 3000:3000 -e PG_HOST=host.docker.internal  daxyonis/school-lunch
+docker run --name school-lunch -p 3000:3000 -e PG_HOST=host.docker.internal -m "300M" --memory-swap "1G" daxyonis/school-lunch
