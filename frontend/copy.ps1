3a9624396e187396c857fb429a72f9d69ad0a3aa
Eva Maciejko <emaciejko@gmail.com>
2022-08-30 14:36:51 -0400

    Update build scripts

--- frontend/copy.ps1
+++ frontend/copy.ps1
@@ -1,2 +1,2 @@
 Remove-Item -Path ..\backend\public\ -Recurse
-Copy-Item -Path public\* -Destination ..\backend\public -PassThru -Recurse
+Copy-Item -Path public\* -Destination ..\backend\public\build -PassThru -Recurse
